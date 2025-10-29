4aba1d8539f793c6e7a8e673b5b5226226e1161f
amnayelamri <53364294+amnayelamri@users.noreply.github.com>
2025-10-29 23:29:19 +0100

    Update Login.jsx

--- frontend/src/components/admin/Login.jsx
+++ frontend/src/components/admin/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './Admin.css';
+import './admin.css';
 
 const API_URL = 'http://localhost:5000/api';
 
